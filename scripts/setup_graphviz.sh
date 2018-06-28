7f2a13c70f7900d908e72eacfe9284d9d89d4327
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-06-28 19:18:14 +0200

    scripts Makefile

---
+++ scripts/setup_graphviz.sh
@@ -0,0 +1,41 @@
+OS=`uname`
+
+LOG_CLEAR='\033[2K'
+LOG_RED='\033[1;31m'
+LOG_GREEN='\033[1;32m'
+LOG_BLUE='\033[1;34m'
+LOG_VIOLET='\033[1;35m'
+LOG_NOCOLOR='\033[0m'
+X='x'
+V='✓'
+
+TITLE=$LOG_CLEAR$LOG_BLUE
+TITLE_MSG="setup prerequisites"
+END=$LOG_NOCOLOR
+
+SETUP=--$LOG_CLEAR$LOG_GREEN$V$LOG_NOCOLOR'\t'setup\ .....................\ $LOG_VIOLET
+KO=--$LOG_CLEAR$LOG_RED$X$LOG_NOCOLOR'\t'setup\ .....................\ $LOG_VIOLET
+KO_MSG="Please check and install manually"
+
+NAME="libSDL2"
+
+if [ "$OS" == Darwine ]
+	then
+	if !(brew ls --versions sdl2 > /dev/null)
+	then
+		echo -e $TITLE$TITLE_MSG$END
+		echo -e $SETUP$NAME$END
+		brew update && brew install sdl2
+	fi
+elif [ "$OS" == Linux ]
+	then
+	if !(dpkg -s libsdl2-dev > /dev/null)
+	then
+		echo -e $TITLE$TITLE_MSG$END
+		echo -e $SETUP$NAME$END
+		sudo apt-get install --yes libsdl2-dev
+	fi
+else
+	echo -e $TITLE$TITLE_MSG$END
+	echo -e $KO$NAME$END'\t'$KO_MSG
+fi
--- scripts/setup_sdl2.sh
+++ scripts/setup_sdl2.sh
@@ -1,35 +1,41 @@
 OS=`uname`
 
-LOG_CLEAR=\\033[2K
-LOG_RED=\\033[1\;31m
-LOG_GREEN=\\033[1\;32m
-LOG_BLUE=\\033[1\;34m
-LOG_VIOLET=\\033[1\;35m
-LOG_NOCOLOR=\\033[0m
+LOG_CLEAR='\033[2K'
+LOG_RED='\033[1;31m'
+LOG_GREEN='\033[1;32m'
+LOG_BLUE='\033[1;34m'
+LOG_VIOLET='\033[1;35m'
+LOG_NOCOLOR='\033[0m'
+X='x'
+V='✓'
+
 TITLE=$LOG_CLEAR$LOG_BLUE
-SETUP=--$LOG_CLEAR$LOG_GREEN✓$LOG_NOCOLOR\tsetup\ ..................$LOG_VIOLET
-KO1=--$LOG_CLEAR$LOG_RED
-KO2=x$LOG_NOCOLOR'\t'setup\ ..................$LOG_VIOLET
-KO=$KO1$KO2
+TITLE_MSG="setup prerequisites"
 END=$LOG_NOCOLOR
+
+SETUP=--$LOG_CLEAR$LOG_GREEN$V$LOG_NOCOLOR'\t'setup\ .....................\ $LOG_VIOLET
+KO=--$LOG_CLEAR$LOG_RED$X$LOG_NOCOLOR'\t'setup\ .....................\ $LOG_VIOLET
+KO_MSG="Please check and install manually"
+
+NAME="graphviz"
 
 if [ "$OS" == Darwine ]
 	then
 	if !(brew ls --versions sdl2 > /dev/null)
 	then
-		echo -e $TITLE setup prerequisites$END
-		echo -e $SETUP libSDL2 $END
-#		brew update && brew install sdl2
+		echo -e $TITLE$TITLE_MSG$END
+		echo -e $SETUP$NAME$END
+		brew update && brew install sdl2
 	fi
-elif [ "$OS" == Linuxe ]
+elif [ "$OS" == Linux ]
 	then
 	if !(dpkg -s libsdl2-dev > /dev/null)
 	then
-		echo -e $TITLE setup prerequisites $END
-		echo -e $SETUP libSDL2 $END
-#		sudo apt-get install --yes libsdl2-dev
+		echo -e $TITLE$TITLE_MSG$END
+		echo -e $SETUP$NAME$END
+		sudo apt-get install --yes libsdl2-dev
 	fi
 else
-	echo -e $TITLE setup prerequisites$END
-	echo -e $KO libSDL2 $END Please check and install manually
+	echo -e $TITLE$TITLE_MSG$END
+	echo -e $KO$NAME$END'\t'$KO_MSG
 fi
