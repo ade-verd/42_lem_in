6ddab1fa488fe4d4740ca7c3175113424271bb9a
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-07-24 19:22:48 +0200

    improve minimalist graph

--- src/lemin/graph.c
+++ src/lemin/graph.c
@@ -6,20 +6,57 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/24 15:30:43 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/07/24 17:30:39 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/07/24 19:22:01 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
-static void	fdprint_links(t_room *current, int fd)
+static int	link_is_on_way(t_map *map, t_room *r1, t_room *r2, int *par)
+{
+	t_way	*ways;
+	int		i;
+
+	ways = map->ways;
+	while (ways && ways->rooms && ways->select)
+	{
+		i = 0;
+		while (ways->rooms[i] && ways->rooms[i + 1])
+		{
+			if (ways->rooms[i] == r1 && ways->rooms[i + 1] == r2)
+			{
+				*par = ways->par;
+				return (1);
+			}
+			if (ways->rooms[i] == r2 && ways->rooms[i + 1] == r1)
+			{
+				*par = ways->par;
+				return (1);
+			}
+			i++;
+		}
+		ways = ways->next;
+	}
+	return (0);
+}
+
+static void	fdprint_links(t_map *map, t_room *current, int fd)
 {
 	t_link	*link;
+	int		par;
 
+	par = 0;
 	link = current->link;
 	while (link)
 	{
-		ft_dprintf(fd, "%s; ", link->to->id);
+		ft_dprintf(fd, "\t%s -- %s", current->id, link->to->id);
+		if (link_is_on_way(map, current, link->to, &par))
+		{
+			ft_dprintf(fd, " [color=red,penwidth=3.0]");
+			if (current == map->start)
+				ft_dprintf(fd, " [label=\"Par: %d\"]", par);
+		}
+		ft_putstr_fd(";\n", fd);
 		link = link->next;
 	}
 }
@@ -31,14 +68,13 @@ static void	fdprint_rooms_links(t_map *map, int fd)
 	cpy = map->head;
 	while (cpy)
 	{
-		if (!cpy->link)
-			ft_dprintf(fd, "\t%s;\n", cpy->id);
+		if (cpy == map->start)
+			ft_dprintf(fd, "\t%s [label=\"START\"][peripheries=2];\n", cpy->id);
+		else if (cpy == map->end)
+			ft_dprintf(fd, "\t%s [label=\"END\"][peripheries=2];\n", cpy->id);
 		else
-		{
-			ft_dprintf(fd, "\t%s -- { ", cpy->id);
-			fdprint_links(cpy, fd);
-			ft_putstr_fd("}\n", fd);
-		}
+			ft_dprintf(fd, "\t%s;\n", cpy->id);
+		fdprint_links(map, cpy, fd);
 		cpy = cpy->next;
 	}
 }
@@ -47,9 +83,11 @@ void	ft_create_graphfile(t_map *map)
 {
 	int		fd;
 
+	ft_remove(DOTFILE);
 	if ((fd = ft_open_fd(DOTFILE, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR)) == -1)
 		ft_error(&map, "ft_create_graphfile", 0);
 	ft_putstr_fd("graph G {\n", fd);
+	ft_putstr_fd("\trankdir=LR;\n", fd);
 	fdprint_rooms_links(map, fd);
 	ft_putstr_fd("}\n", fd);
 	if ((ft_close_fd(fd)) == -1)
