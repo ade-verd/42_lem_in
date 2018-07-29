2048fa3d2e9a424dc1d221e07728fbba286f1808
Aurelien DE VERDILHAC <ade-verd@student.42.fr>
2018-07-29 15:46:26 +0200

    fix no start

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ade-verd <ade-verd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/17 16:27:33 by ade-verd          #+#    #+#             */
-/*   Updated: 2018/07/26 15:05:01 by ade-verd         ###   ########.fr       */
+/*   Updated: 2018/07/29 15:46:09 by ade-verd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ int		main(int ac, char **av)
 		ft_read_args(&map, ac, av);
 	ft_deal_help_debug(map);
 	ft_read_fd(&map);
-	find_issues(map, map->start, 0);
 	ft_check_validity(map);
+	find_issues(map, map->start, 0);
+	if (!ft_count_issues(map))
+		ft_error(&map, "No issue", 0);
 	ft_sort_issues(map);
 	ft_select_issues(map);
 	ft_sort_selected_issues(map);
